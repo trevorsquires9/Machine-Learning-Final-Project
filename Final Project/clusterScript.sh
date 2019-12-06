98f19f8c3fd904ab6c2382011691600f883b5df4
Trevor Squires <tsquire@clemson.edu>
2019-12-06 08:33:29 -0500

    Created cluster script

---
+++ Final Project/clusterScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#PBS -N math9880finalproject
+#PBS -l select=1:ncpus=8:mem=64gb
+#PBS -l walltime=72:00:00
+#PBS -m abe
+#PBS -M tsquire@g.clemson.edu
+#PBS -j oe
+#PBS -o trs.error
+
+module add matlab/2018b
+
+cd $PBS_O_WORKDIR
+
+taskset -c 0-$(($OMP_NUM_THREADS-1)) matlab -nodisplay -nodesktop -nosplash -r trsScript
+
